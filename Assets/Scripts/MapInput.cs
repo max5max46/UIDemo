ec7a4967a5502b34d5daa97a3d0d8a35b7f5a903
Dylan Adams <dylan.adams.2016@outlook.com>
2023-10-18 17:10:03 -0300

    updates level and map

--- Assets/Scripts/MapInput.cs
+++ Assets/Scripts/MapInput.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,21 +8,36 @@ public class MapInput : MonoBehaviour
 {
     
     [SerializeField] private GameObject map;
+    private RectTransform mapTrans;
+    private float lerpTarget = -2000;
+    private bool goingUp = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        map.SetActive(false);
+        mapTrans = map.GetComponent<RectTransform>();
+        mapTrans.localPosition = new Vector3(0, lerpTarget, 0);
+        map.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(mapTrans.localPosition.x + " " + mapTrans.localPosition.y + " " + mapTrans.localPosition.z);
+        Debug.Log(lerpTarget);
+
         if (Input.GetKeyDown(KeyCode.M))
-        {
-            if (!map.activeInHierarchy) 
-                map.SetActive(true);
+            if (!goingUp)
+            {
+                lerpTarget = -96;
+                goingUp = true;
+            }
             else
-                map.SetActive(false);
-        }
+            {
+                lerpTarget = -2000;
+                goingUp = false;
+            }
+
+        mapTrans.localPosition = new Vector3(0, Mathf.Lerp(mapTrans.localPosition.y, lerpTarget, Time.deltaTime * 10), 0);
     }
 }
